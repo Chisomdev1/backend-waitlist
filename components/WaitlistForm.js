bd991e38ee6b25111d0a068fa80c691e35e3d4a6
Chisomdev1 <chisomchidike03@gmail.com>
2025-05-03 20:47:44 +0100

    working

--- components/WaitlistForm.js
+++ components/WaitlistForm.js
@@ -64,7 +64,7 @@ export default function WaitlistForm() {
       </form>
 
       {/* Alert Modal */}
-      {!showAlert && (
+      {showAlert && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white rounded-lg p-6 shadow-lg max-w-sm w-full">
             <GiCancel />
